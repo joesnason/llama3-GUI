d9ae41248e626d0f98b45efaafab1e799e6380ae
joesnason <joesnason@gmail.com>
2024-04-12 15:31:43 +0800

    change to steaming version to improve UX

--- llama2_UI_version.py
+++ llama2_UI_version.py
@@ -2,37 +2,49 @@ import tkinter as tk
 from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-class MyOutputParser(StrOutputParser):
-    def parse(self, text):
-        return text.replace('Assistant: ', '').replace('\n', '')
+
+
+
+class LLMStreamingCallback(StreamingStdOutCallbackHandler):
+    def __init__(self):
+        super().__init__()
+        #self.text = ""
+    def on_llm_new_token(self, token: str, **kwargs: any):
+        #self.text += token
+        token= token.replace('\n', '')
+        response["text"] = response["text"] + token
+        #print(token)
+        window.update()
 
 
 def HelloMsg():
     system_prompt = textbox_systemprompt.get("1.0", tk.END)
     user_prompt = input_textedit.get()
-    response["text"] = response["text"] + "\n" + "user: " + user_prompt
+    response["text"] = response["text"] + "\n" + "User: " + user_prompt
     prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt),    
     ("user", user_prompt),
 ])
-    chain = prompt | llm | output_parser
-    context = response["text"] + "\n" + "AI: "+  chain.invoke({"user_prompt": user_prompt}) 
-    response.config(text=context, anchor="w")
+    chain = prompt | llm  
+    context = response["text"] + "\n"  + "AI: " # +  chain.invoke({"user_prompt": user_prompt}) 
+    response.config(text=context, anchor="nw")
+    chain.invoke({"user_prompt": user_prompt})
 
 window = tk.Tk()
 window.title("Tkinter Test")
 window.geometry("800x600")
 
-llm = Ollama(model='llama2')
-output_parser = MyOutputParser()
+llm = Ollama(model='llama2', callback_manager=CallbackManager([LLMStreamingCallback()]))
 
 
 title = tk.Label(window, text="Local Llama2  test", font=("Arial", 24), bg="yellow")
 title.pack()
 
 textbox_systemprompt = tk.Text(window,  height=5, width=100, font=("Arial", 12))
-textbox_systemprompt.insert(tk.END, "你是一個英翻中的專家。請翻譯下列英文句子")
+textbox_systemprompt.insert(tk.END, "你是一個英翻中的專家。請翻譯下列英文句子成繁體中文")
 textbox_systemprompt.pack()
 
 
